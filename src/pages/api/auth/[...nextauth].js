bea885bf64939c534cc57a43014a58ee8295312d
NSGOmega <emersontrickz@gmail.com>
2023-03-29 19:59:48 -0400

    fsfklnsdgopkdns gklds

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,15 @@
 import NextAuth from "next-auth/next";
 import PatreonProvider from "next-auth/providers/patreon"
-import DiscordProvider from "next-auth/providers/discord"
 
 export const authOptions = {
     providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET
+        // DiscordProvider({
+        //     clientId: process.env.DISCORD_CLIENT_ID,
+        //     clientSecret: process.env.DISCORD_CLIENT_SECRET
+        // }),
+        PatreonProvider({
+            clientId: process.env.PATREON_CLIENT_ID,
+            clientSecret: process.env.PATREON_CLIENT_SECRET
         })
     ],
     secret: process.env.JWT_SECRET
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,11 +3,12 @@ import { useSession, signIn, signOut } from 'next-auth/react'
 
 export default function Login() {
     const { data: session } = useSession()
+    console.log(session);
 
     if (session) {
 
         return (
-            <div className="text-center">
+            <div className="mt-10 text-center">
                 <img className="mx-auto" src={session.user.image}></img>
                 <h1 className="text-lg text-blue-400">Signed in as {session.user.name} </h1><br />
                 <button className="btn" onClick={() => signOut()}>Sign out</button>
@@ -15,8 +16,8 @@ export default function Login() {
         )
     }
     return (
-        <div className="text-center">
-            Not signed in <br />
+        <div className="mt-20 text-center">
+            <br />
             <button className="btn" onClick={() => signIn()}>Sign in</button>
         </div>
     )
