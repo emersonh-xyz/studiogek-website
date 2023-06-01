78afbf3302478f45a27e13e07c86732cfe10feed
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-06-01 13:30:49 -0400

    Hide YouTube section on mobile (temp fix)

--- src/components/Layouts/Pages/Home/Content.js
+++ src/components/Layouts/Pages/Home/Content.js
@@ -11,7 +11,10 @@ export const Content = () => (
 
             <PatreonContent />
 
-            <YoutubeContent />
+            {/*Lets just hide this section until I find a better fix*/}
+            <Container css={{ visibility: "hidden", h: 0, '@xs': { visibility: "visible", h: 'auto' } }}>
+                <YoutubeContent />
+            </Container>
 
             <ArtContent />
 
