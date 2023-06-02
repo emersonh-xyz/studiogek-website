d08918c86075d442dff7af36c5c864f69617af7d
NSGOmega <emersontrickz@gmail.com>
2023-06-02 19:51:05 -0400

    Post authorization for uncut tier

--- src/pages/api/posts/post/index.js
+++ src/pages/api/posts/post/index.js
@@ -28,11 +28,17 @@ export default async function handler(req, res) {
         // console.log("user tier:", userTier)
         // console.log("post tier:", postTier.id)
 
-        // Check if the tier matches the post tier, if so give post
+        // Check if the tier matches the post tier, if so send post back
         if (userTier.weight >= postTier.weight) {
             res.status(200).json({ data: post, status: 200 })
             return;
-        } else {
+
+            // If post is Gek and user is Uncut and a week passed since post date 
+        } else if (postTier.id === "9384773" && userTier.id === "9384741" && hasAWeekPassed(post.timestamp)) {
+            res.status(200).json({ data: post, status: 200 })
+            return;
+        }
+        else {
             res.status(401).json({ message: "Unauthorized tier", data: postTier.display, status: 401 })
             return;
         }
@@ -44,4 +50,25 @@ export default async function handler(req, res) {
     }
 
 
+}
+
+function hasAWeekPassed(postDate) {
+    // Get the current timestamp
+    var now = new Date();
+
+    // Convert the provided timestamp to a Date object
+    var providedTimestamp = new Date(postDate);
+
+    // Calculate the difference in milliseconds
+    var timeDifference = now.getTime() - providedTimestamp.getTime();
+
+    // Define the number of milliseconds in a week
+    var millisecondsInWeek = 7 * 24 * 60 * 60 * 1000;
+
+    // Check if a week has passed
+    if (timeDifference >= millisecondsInWeek) {
+        return true;
+    } else {
+        return false;
+    }
 }
