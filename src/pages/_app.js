6752e1235a886d87b70cddcc21593645973c4e88
NSGOmega <emersontrickz@gmail.com>
2023-06-02 18:57:36 -0400

    Vercel anlaytics init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react"
 import { createTheme, NextUIProvider } from "@nextui-org/react"
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { SSRProvider } from '@react-aria/ssr';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({
 
@@ -41,6 +42,7 @@ export default function App({
         >
           <NextUIProvider >
             <Component {...pageProps} />
+            <Analytics />
           </NextUIProvider>
         </NextThemesProvider>
       </SessionProvider>
