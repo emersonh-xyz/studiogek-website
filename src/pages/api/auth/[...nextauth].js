efb72dd089835e6561c0df2adce78e4a39b04f5c
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-06-29 15:49:18 -0400

    okkkkk

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,10 +4,6 @@ import PatreonProvider from "next-auth/providers/patreon"
 
 export const authOptions = {
     providers: [
-        // DiscordProvider({
-        //     clientId: process.env.DISCORD_CLIENT_ID,
-        //     clientSecret: process.env.DISCORD_CLIENT_SECRET
-        // }),
         PatreonProvider({
             clientId: process.env.PATREON_CLIENT_ID,
             clientSecret: process.env.PATREON_CLIENT_SECRET
