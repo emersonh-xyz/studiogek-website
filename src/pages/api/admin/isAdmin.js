1498ed304107411ed6e87ab3db6af6d9b514c6a0
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-07-14 23:38:24 -0400

    Create isAdmin.js

---
+++ src/pages/api/admin/isAdmin.js
@@ -0,0 +1,28 @@
+import { getToken } from 'next-auth/jwt'
+
+export default async (req, res) => {
+
+    const secret = process.env.NEXTAUTH_SECRET;
+    const token = await getToken({ req, secret: secret })
+
+    try {
+
+        console.log(token?.role)
+
+        if (token?.role === "admin") {
+            return res.status(200).json({
+                isAdmin: true
+            })
+        }
+
+        return res.status(401).json({
+            isAdmin: false
+        })
+
+    } catch (e) {
+        return res.status(400).json({
+            status: e.message
+        })
+    }
+}
+
